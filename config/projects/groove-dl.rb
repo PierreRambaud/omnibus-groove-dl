2a21680c14b84f6da30c11ebe2a8baff7ff22a3c
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-12-13 12:25:43 +0100

    begin project and software configuration

--- config/projects/groove-dl.rb
+++ config/projects/groove-dl.rb
@@ -1,11 +1,21 @@
 
 name 'groove-dl'
-maintainer 'CHANGE ME'
-homepage 'https://CHANGE-ME.com'
+maintainer 'Pierre Rambaud'
+homepage 'https://github.com/PierreRambaud'
 
-install_dir     '/opt/groove-dl'
 build_version   '0.1.0'
 build_iteration 1
+
+
+if windows?
+  # NOTE: Ruby DevKit fundamentally CANNOT be installed into "Program Files"
+  # Native gems will use gcc which will barf on files with spaces,
+  # which is only fixable if everyone in the world fixes their Makefiles
+  install_dir "#{default_root}/opscode/#{name}"
+  package_name "groove-dl"
+else
+  install_dir "#{default_root}/#{name}"
+end
 
 dependency "preparation"
 dependency "version-manifest"
@@ -13,3 +23,7 @@ dependency "groove-dl"
 
 exclude '\.git*'
 exclude 'bundler\/git'
+
+package :msi do
+  upgrade_code '140e8f3f-854a-4a2d-a073-6d22e75fdb28'
+end
--- config/software/groove-dl.rb
+++ config/software/groove-dl.rb
@@ -8,16 +8,53 @@
 name "groove-dl"
 default_version "0.1.0"
 
-dependency "ruby"
-dependency "rubygems"
+if windows?
+  dependency 'ruby-windows'
+  dependency 'openssl-windows'
+else
+  dependency "ruby"
+end
+
 dependency "bundler"
+dependency 'rubygems'
 
 source :git => "git://github.com/PierreRambaud/groove-dl.git"
 
 relative_path "groove-dl"
 
 build do
-  bundle "install --path=#{install_dir}/embedded/service/gem"
-  command "mkdir -p #{install_dir}/embedded/service/groove-dl"
-  command "#{install_dir}/embedded/bin/rsync -a --delete --exclude=.git/*** --exclude=.gitignore ./ #{install_dir}/embedded/service/groove-dl/"
+  if windows?
+    {
+      'tar.exe' => 'bsdtar.exe',
+      'libarchive-2.dll' => 'libarchive-2.dll',
+      'libexpat-1.dll' => 'libexpat-1.dll',
+      'liblzma-1.dll' => 'liblzma-1.dll',
+      'libbz2-2.dll' => 'libbz2-2.dll',
+      'libz-1.dll' => 'libz-1.dll',
+    }.each do |target, to|
+      copy "#{install_dir}/embedded/mingw/bin/#{to}", "#{install_dir}/bin/#{target}"
+    end
+
+    gem 'build groove-dl-x86-mingw32.gemspec', env: env
+    gem 'install groove-dl*mingw32.gem  --no-ri --no-rdoc --verbose', env: env
+    bundle 'install --without server docgen', env: env
+    block 'Build Event Log Dll' do
+      Dir.chdir software.project_dir do
+        rake = windows_safe_path("#{install_dir}/embedded/bin/rake")
+        %x(#{rake} -rdevkit build_eventlog) if File.exists? "#{software.project_dir}/ext/win32-eventlog"
+      end
+    end
+  else
+    bundle 'install --without server docgen', env: env
+    gem 'build groove-dl.gemspec', env: env
+    gem 'install groove*.gem --no-ri --no-rdoc', env: env
+  end
+
+  delete "#{install_dir}/embedded/docs"
+  delete "#{install_dir}/embedded/share/man"
+  delete "#{install_dir}/embedded/share/doc"
+  delete "#{install_dir}/embedded/share/gtk-doc"
+  delete "#{install_dir}/embedded/ssl/man"
+  delete "#{install_dir}/embedded/man"
+  delete "#{install_dir}/embedded/info"
 end
