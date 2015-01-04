f71431082b5464c70e08a4110194c5cd9e325699
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-04 13:25:45 +0100

    Compile libpng12 instead of 15

---
+++ config/software/libpng.rb
@@ -0,0 +1,20 @@
+name 'libpng'
+default_version '1.2.52'
+
+dependency 'zlib'
+
+source(url: "http://downloads.sourceforge.net/libpng/libpng-#{version}.tar.gz",
+       md5: '89dbefdbe268cdfeb5525adde805272f')
+
+relative_path "#{name}-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  command('./configure' \
+          " --prefix=#{install_dir}/embedded" \
+          " --with-zlib-prefix=#{install_dir}/embedded", env: env)
+
+  make "-j #{workers}", env: env
+  make "install", env: env
+end
