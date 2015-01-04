d7085864ed9ffd71eb51794feaa124f9cef3a0cf
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-04 12:52:21 +0100

    Fix iconv compilation

--- config/software/libiconv.rb
+++ config/software/libiconv.rb
@@ -9,9 +9,18 @@ relative_path "#{name}-#{version}"
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+  env.merge!(
+    'LDFLAGS' => "-L#{install_dir}/embedded/lib" \
+    " -I#{install_dir}/embedded/include",
+    'CFLAGS' => "-L#{install_dir}/embedded/lib" \
+    " -I#{install_dir}/embedded/include",
+    'LD_RUN_PATH' => "#{install_dir}/embedded/lib"
+  )
 
   configure_command = './configure' \
                       " --prefix=#{install_dir}/embedded"
+
+  patch source: 'libiconv-1.14_srclib_stdio.in.h-remove-gets-declarations.patch'
 
   command configure_command, env: env
 
--- config/software/libxcb.rb
+++ config/software/libxcb.rb
@@ -13,13 +13,15 @@ build do
   cmd = ['./configure',
          "--prefix=#{install_dir}/embedded"
         ].join(' ')
-  env = {
+
+  env = with_standard_compiler_flags(with_embedded_path)
+  env.merge!(
     'LDFLAGS' => "-L#{install_dir}/embedded/lib" \
     " -I#{install_dir}/embedded/include",
     'CFLAGS' => "-L#{install_dir}/embedded/lib" \
     " -I#{install_dir}/embedded/include",
     'LD_RUN_PATH' => "#{install_dir}/embedded/lib"
-  }
+  )
 
   command(cmd,
           env: env)
