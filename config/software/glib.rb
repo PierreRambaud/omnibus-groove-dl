669ac5a69e10d8fc96c7e90d29b92b96ea27fe14
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-07 17:26:51 +0100

    Remove useles environment variables

--- config/software/glib.rb
+++ config/software/glib.rb
@@ -16,13 +16,6 @@ build do
         ].join(' ')
 
   env = with_standard_compiler_flags(with_embedded_path)
-  env.merge!(
-    'LDFLAGS' => "-L#{install_dir}/embedded/lib" \
-    " -I#{install_dir}/embedded/include",
-    'CFLAGS' => "-L#{install_dir}/embedded/lib" \
-    " -I#{install_dir}/embedded/include",
-    'LD_RUN_PATH' => "#{install_dir}/embedded/lib"
-  )
 
   command(cmd, env: env)
   make("-j #{workers}", env: env)
