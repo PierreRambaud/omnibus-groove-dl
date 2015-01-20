0e9fac44bd8c6d7aaed82a52fcd50ce2967bd58c
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-20 12:05:02 +0100

    Remove useless directories

--- config/software/groove-dl.rb
+++ config/software/groove-dl.rb
@@ -60,10 +60,13 @@ build do
   appbundle 'groove-dl'
 
   delete "#{install_dir}/embedded/docs"
+  delete "#{install_dir}/embedded/var"
   delete "#{install_dir}/embedded/share/man"
   delete "#{install_dir}/embedded/share/doc"
   delete "#{install_dir}/embedded/share/gtk-doc"
   delete "#{install_dir}/embedded/ssl/man"
   delete "#{install_dir}/embedded/man"
   delete "#{install_dir}/embedded/info"
+  delete "#{install_dir}/embedded/libexec"
+  delete "#{install_dir}/embedded/etc"
 end
